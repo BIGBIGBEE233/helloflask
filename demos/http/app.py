8b44ac46d29a7f86ef10953782a87802f5b08779
BIGBEE <316182952@qq.com>
2022-11-29 16:39:02 +0800

    learn flask tenth day

--- demos/http/app.py
+++ demos/http/app.py
@@ -208,8 +208,7 @@ def do_something():
 def is_safe_url(target):
     ref_url = urlparse(request.host_url)
     test_url = urlparse(urljoin(request.host_url, target))
-    return test_url.scheme in ('http', 'https') and \
-           ref_url.netloc == test_url.netloc
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
 
 
 def redirect_back(default='hello', **kwargs):
@@ -217,5 +216,5 @@ def redirect_back(default='hello', **kwargs):
         if not target:
             continue
         if is_safe_url(target):
-            return redirect(target)
+            return redirect(target)  # target是绝对路径
     return redirect(url_for(default, **kwargs))
